e74276d0d55462ce5402c303348e5925754c12df
Waghib <Waghibahmad30@gmail.com>
2024-11-26 18:41:03 +0500

    updated firebase config files

--- src/config/firebaseConfig.js
+++ src/config/firebaseConfig.js
@@ -1,15 +1,7 @@
-import {
-  FIREBASE_API_KEY,
-  FIREBASE_PROJECT_ID,
-  FIREBASE_APP_ID,
-  FIREBASE_STORAGE_BUCKET,
-  FIREBASE_MESSAGING_SENDER_ID,
-} from '@env';
-
 export const firebaseConfig = {
-  apiKey: FIREBASE_API_KEY,
-  projectId: FIREBASE_PROJECT_ID,
-  appId: FIREBASE_APP_ID,
-  storageBucket: FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
-}; 
+    apiKey: FIREBASE_API_KEY,
+    projectId: FIREBASE_PROJECT_ID,
+    appId: FIREBASE_APP_ID,
+    storageBucket: FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
+};
