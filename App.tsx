6d643e61e1cc300b691ff7e9ec4da6a7ded78b96
Waghib <Waghibahmad30@gmail.com>
2024-11-27 23:00:16 +0500

    updated types for screens in app.tsx

--- App.tsx
+++ App.tsx
@@ -1,19 +1,18 @@
-import { StyleSheet } from 'react-native'
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import SignInScreen from './src/screens/SignInScreen';
 import SignUpScreen from './src/screens/SignUpScreen';
 import OTPVerificationScreen from './src/screens/OTPVerificationScreen';
-import './src/config/firebase';
+import { RootStackParamList } from './types';
 
-const Stack = createNativeStackNavigator();
+const Stack = createNativeStackNavigator<RootStackParamList>();
 
-const App = () => {
+const App: React.FC = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator 
-        initialRouteName="SignUp"
+        initialRouteName='SignUp'
         screenOptions={{ 
           headerShown: false,
         }}
@@ -23,9 +22,7 @@ const App = () => {
         <Stack.Screen name="OTPVerification" component={OTPVerificationScreen} />
       </Stack.Navigator>
     </NavigationContainer>
-  )
-}
+  );
+};
 
 export default App;
-
-const styles = StyleSheet.create({});
