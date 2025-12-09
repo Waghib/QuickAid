c2a3b7aead074e9d166b8e52ae308b96eeab36b4
Sarim <dev@Devs-MacBook-Pro-3.local>
2025-12-09 15:58:16 +0500

    Versions updated successfully

--- android/app/src/main/java/com/quickaid/MainApplication.kt
+++ android/app/src/main/java/com/quickaid/MainApplication.kt
@@ -10,6 +10,8 @@ import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.load
 import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
 import com.facebook.react.defaults.DefaultReactNativeHost
 import com.facebook.soloader.SoLoader
+// [FIX 1] Add this specific import for RN 0.76+
+import com.facebook.react.soloader.OpenSourceMergedSoMapping
 
 class MainApplication : Application(), ReactApplication {
 
@@ -34,7 +36,11 @@ class MainApplication : Application(), ReactApplication {
 
   override fun onCreate() {
     super.onCreate()
-    SoLoader.init(this, false)
+    
+    // [FIX 2] Update SoLoader to use the new Merged Mapping
+    // Old line was: SoLoader.init(this, false)
+    SoLoader.init(this, OpenSourceMergedSoMapping)
+    
     if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
       // If you opted-in for the New Architecture, we load the native entry point for this app.
       load()
--- babel.config.js
+++ babel.config.js
@@ -1,20 +1,12 @@
 module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
-  plugins: ['@babel/plugin-transform-private-methods'],
- plugins: [
-  '@babel/plugin-transform-private-methods',
-  '@babel/plugin-transform-class-properties',
-  '@babel/plugin-transform-private-property-in-object',
-   [
-     'module:react-native-dotenv',
-     {
-       envName: 'APP_ENV',
-       moduleName: '@env',
-       path: '.env',
-       safe: false,
-       allowUndefined: true,
-       blocklist: null,
-       allowlist: null,
+  presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    [
+      'module:react-native-dotenv',
+      {
+        envName: 'APP_ENV',
+        moduleName: '@env',
+        path: '.env',
       },
     ],
   ],
